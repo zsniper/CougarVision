193866c455668a2b28d5e4b0596e02f6cc37f265
Shun Yu Rao <shunyu.rao@gmail.com>
2014-02-05 19:14:24 -0500

    Big commit - menu & interface The first of many big interface updates.
    -Added basic slide menu using Lucas's design
    -Now uses fragments instead of separate activities for easier design and
    consistent styles
    -Icon names and fragments are temporary and subject to change

---
+++ src/ca/cb/cc/cv/CVFragment.java
@@ -0,0 +1,21 @@
+package ca.cb.cc.cv;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+ 
+public class CVFragment extends Fragment {
+     
+    public CVFragment(){}
+     
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+  
+        View rootView = inflater.inflate(R.layout.fragment_cv, container, false);
+          
+        return rootView;
+    }
+}
---
+++ src/ca/cb/cc/cv/SettingsFragment.java
@@ -0,0 +1,21 @@
+package ca.cb.cc.cv;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+ 
+public class SettingsFragment extends Fragment {
+     
+    public SettingsFragment(){}
+     
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+  
+        View rootView = inflater.inflate(R.layout.fragment_settings, container, false);
+          
+        return rootView;
+    }
+}
